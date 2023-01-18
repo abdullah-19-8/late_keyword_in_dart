553f54f06336cd3adb11e201d1d41d55756773d7
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2023-01-18 09:20:26 +0300

    late initialization

---
+++ bin/lazy_initialization_and_errors.dart
@@ -0,0 +1,19 @@
+// when we want to use a variable, we can use the 'late' keyword
+// to tell the compiler that we will initialize the variable later, it is not null
+// and we don't want to initialize it now
+// this is called 'lazy initialization'
+
+late String name; // this is a lazy initialization
+
+// if we didn't use the 'late' keyword, we would get an error
+
+void main(List<String> args) {
+  // if we try to print the variable, we get an exception
+  try{ // we use a try-catch block to catch the exception, and print it, instead of crashing the program
+    print(name); // Exception: LateInitializationError: Field 'name' has not been initialized.
+  } catch(e){
+    print(e);
+  }
+  name = "John"; // we initialize the variable
+  print(name); // we print the variable
+}
---
+++ bin/lazy_initialization_to_function_result.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // we can use the 'late' keyword with functions too
+  // when we call the function, it will be initialized
+  // and the result will be stored in the variable
+  print('before calling the function');
+  late String name =
+      getName(); // this function will be called when we use the variable, not now,
+  //  if we remove the 'late' keyword, it will be called now
+  print('after calling the function');
+  print(name);
+}
+
+String getName() {
+  print('inside the function');
+  return "John";
+}
